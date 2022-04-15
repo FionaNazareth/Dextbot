5bced4fda15a216de719f4d5f636711009d10aa0
Fiona Reginald Nazareth <32682507+FionaNazareth@users.noreply.github.com>
2022-04-14 23:32:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,33 +5,30 @@ from flask import Flask, render_template, request
 
 # variables
 name = None
+flag = 0
+# bot = None
 answers = []
 
 # main
 app = Flask(__name__)
 
-bot = ChatBot("Dexter", logic_adapters=['chatterbot.logic.BestMatch', 'chatterbot.logic.MathematicalEvaluation'])
-
-# Training bot
-# trainer = ChatterBotCorpusTrainer(bot)
-# trainer.train("chatterbot.corpus.english")
-bot.set_trainer(ChatterBotCorpusTrainer)
-bot.train("chatterbot.corpus.english")
-
 
 @app.route('/')
 def home():
-    answers = []
+#     bot = BotInit()
     return render_template('index.html', ans=answers)
 
 
 @app.route('/answer', methods=['POST'])
 def answer():
+    if flag == 0:
+        bot = BotInit()
+        flag = 1
+        
     answers.append(("Human", request.form.get('query')))
     if request.form.get('query').lower().startswith("bye"):
         home()
     else:
-        # response = Chatbot(request.form.get('query'))
         response = bot.get_response(request.form.get('query'))
         answers.append(
             (
@@ -40,6 +37,20 @@ def answer():
             )
         )
     return render_template('index.html', ans=answers)
+
+
+def BotInit():
+    # Intialize the bot
+    bot_1 = ChatBot("Dexter",
+                  logic_adapters=['chatterbot.logic.BestMatch',
+                                  'chatterbot.logic.MathematicalEvaluation'])
+    # Training bot
+    #trainer = ChatterBotCorpusTrainer(bot)
+    #trainer.train("chatterbot.corpus.english")
+    bot_1.set_trainer(ChatterBotCorpusTrainer)
+    bot_1.train("chatterbot.corpus.english")
+    print(bot_1)
+    return bot_1
 
 if __name__ == '__main__':
     app.run(debug=True)
