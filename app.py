7efbfa23afa9c4d1d85d8a2b40e627bf7a8704ce
Fiona Reginald Nazareth <32682507+FionaNazareth@users.noreply.github.com>
2022-04-16 00:55:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,35 @@
-# Importing Libraries
+from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-from flask import Flask, render_template, request
 
-# variables
-name = None
-flag = 0
-bot = None
-answers = []
-
-# main
 app = Flask(__name__)
 
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+answers = []
 
-@app.route('/')
+@app.route("/")
 def home():
-    BotInit()
-    answers.append(bot)
-    print("home:", bot)
-    return render_template('index.html', ans=answers)
+    return render_template("index.html")
 
 
-
-@app.route('/answer', methods=['POST'])
-   
-def answer():
-    global bot
-    print("Answer:", bot)
+@app.route('/reply', methods=['POST'])
+def reply():
     print(answers)
-    answers.append(("Human", request.form.get('query')))
-    if request.form.get('query').lower().startswith("bye"):
-        home()
-    else:
-        response = bot.get_response(request.form.get('query'))
-        answers.append(
-            (
-                "Dexter",
-                response
-            )
-        )
-    return render_template('index.html', ans=answers)
+    answers.append(english_bot)
+
+    query = request.form.get('query')
+    answers.append(("Human", query))
+    print("User query:", query)
+
+    response = answers[0].get_response(query)
+    print("Bot response:", response)
+    answers.append(("Bot", response))
+
+    return render_template('index.html', response=answers[1:])
 
 
-def BotInit():
-    global bot
-    # Intialize the bot
-    bot = ChatBot("Dexter",
-                  logic_adapters=['chatterbot.logic.BestMatch',
-                                  'chatterbot.logic.MathematicalEvaluation'], storage_adapter="chatterbot.storage.SQLStorageAdapter")
-    # Training bot
-    #trainer = ChatterBotCorpusTrainer(bot)
-    #trainer.train("chatterbot.corpus.english")
-    bot.set_trainer(ChatterBotCorpusTrainer)
-    bot.train("chatterbot.corpus.english")
-    print("BotInit:", bot)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
